115d00ac3887b7f6fb6fb086cd36c7c457ee33f3
Bradley Troll <bradleytroll@gmail.com>
2023-05-09 09:27:45 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // code your solution here
+function superbowlWin(array){
+    const winRecord = array.find(record => record.result === "W");
+    return winRecord ? winRecord.year : undefined;
+};
+
+
+ 
