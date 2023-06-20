931c0f66d8695bc69fc5fe307ca4398121a6c9cf
Bradley Troll <bradleytroll@gmail.com>
2023-06-20 10:49:40 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-// code your solution here
-function superbowlWin(array){
-    const winRecord = array.find(record => record.result === "W");
-    return winRecord ? winRecord.year : undefined;
-};
+// Redoing old labs for extra practice. New code written here with old code commented out below.
+
+function superbowlWin(array) {
+   for (const entry of array) {
+    if (entry.result === "W") {
+        return entry.year;
+    }
+   }
+   return undefined;
+}
+
+
+// // code your solution here
+// function superbowlWin(array){
+//     const winRecord = array.find(record => record.result === "W");
+//     return winRecord ? winRecord.year : undefined;
+// };
 
 
  
